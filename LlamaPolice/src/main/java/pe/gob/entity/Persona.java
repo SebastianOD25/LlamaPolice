972d201d68378b74db4a6cb26b96bd4c609cb578
sbast <sbast@192.168.1.136>
2020-05-14 17:48:46 -0500

    ,,,

--- LlamaPolice/src/main/java/pe/gob/entity/Persona.java
+++ LlamaPolice/src/main/java/pe/gob/entity/Persona.java
@@ -16,7 +16,6 @@ public class Persona implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPersona;
 	
 	@Column(name = "nombrePersona",nullable = false,length = 30)
